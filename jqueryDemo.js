2037e9c0ddb7037110546ffb43c1ef28b814fe44
Nikolas9896 <nhlynka@gmail.com>
2019-08-30 13:11:35 +0300

    create jQuery file for training

---
+++ jqueryDemo.js
@@ -0,0 +1,11 @@
+$("h1").css("border","1px solid orange");
+$("h1").css("margin","0");
+$("h1").css("padding","0");
+$("h1").css("padding","0");
+var styles = {
+	color: "steelblue",
+	background: "#232323",
+	border: "2px solid gray"
+};
+$("h1").css(styles);
+$("li").css(styles);
