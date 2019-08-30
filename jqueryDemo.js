b20199c4c1357ef5b74b3729e5b413095d28760c
Nikolas9896 <nhlynka@gmail.com>
2019-08-30 13:37:08 +0300

    add 2 simple exercises

--- jqueryDemo.js
+++ jqueryDemo.js
@@ -1,3 +1,4 @@
+//first exercise
 $("h1").css("border","1px solid orange");
 $("h1").css("margin","0");
 $("h1").css("padding","0");
@@ -9,3 +10,14 @@ var styles = {
 };
 $("h1").css(styles);
 $("li").css(styles);
+
+//second exercise
+
+$("div").css("background", "#232323");
+$(".highlight").css("width", "200px");
+$(".highlight").css("color", "white");
+$(".highlight").css("color", "green");
+$(".third").css("border", "2px solid orange");
+$("#third").css("border", "2px solid orange");
+$("#third").css("color", "lightgray");
+$("div:first").css("color", "steelblue");
