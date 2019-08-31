f3b81dd94174a1aee1d42fa001a1a21d49e32a9a
Nikolas9896 <nhlynka@gmail.com>
2019-08-31 21:27:28 +0300

    add on method for li click and span click - because in after click we cann't manipulate with new todos

--- TodoProject/assets/js/todos.js
+++ TodoProject/assets/js/todos.js
@@ -1,10 +1,10 @@
 // Check Off Specific Todos By Clicking
-$("li").click(function(){
+$("ul").on("click","li",function(){
    $(this).toggleClass("completed");
 });
 
 //Click on X to delete Todo
-$("span").click(function(event){
+$("ul").on("click","span",function(event){
     $(this).parent().fadeOut(500, function(){ // ($this) = span .parent() = li remove span with parent element
         $(this).remove(); // ($this) = li
     }); 
@@ -14,11 +14,12 @@ $("span").click(function(event){
 //Add new todo
 $("input[type='text']").keypress(function(event){
 
-   if(event.which === 13)
+   if(event.which === 13)// 13 - it's code of ENTER key
    {
        //grabbing new todo text from input
       var todoText = $(this).val();
-        $(this).val("");
+      // clear input  
+      $(this).val("");
       // create new li and add to ul
      
      $("ul").append("<li><span>X</span> " + todoText + "</li>");
