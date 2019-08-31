13cbe1c629e5cf75a2914bebf61c8fdd90b739f9
Nikolas9896 <nhlynka@gmail.com>
2019-08-31 16:47:01 +0300

    add completed class and toggleClass in todos.js with style color gray and text-decoration line-through

--- TodoProject/assets/js/todos.js
+++ TodoProject/assets/js/todos.js
@@ -1,18 +1,4 @@
 // Check Off Specific Todos By Clicking
 $("li").click(function(){
-    if($(this).css("color") === "rgb(128, 128, 128)")
-    {
-        $(this).css({
-            color: "rgb(0, 0, 0)",
-            textDecoration: "none"
-        });
-    }
-    else
-    {
-        $(this).css({
-            color: "rgb(128, 128, 128)",
-            textDecoration: "line-through"
-        });
-    }
-
+   $(this).toggleClass("completed");
 });
