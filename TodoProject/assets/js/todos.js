10bd2d97fe7379f0b792ef7619d07fabff06aaed
Nikolas9896 <nhlynka@gmail.com>
2019-08-31 17:04:26 +0300

    Click on X to delete Todo with fadeOut and stopPropagation

--- TodoProject/assets/js/todos.js
+++ TodoProject/assets/js/todos.js
@@ -2,3 +2,12 @@
 $("li").click(function(){
    $(this).toggleClass("completed");
 });
+
+//Click on X to delete Todo
+$("span").click(function(event){
+    $(this).parent().fadeOut(500, function(){ // ($this) = span .parent() = li remove span with parent element
+        $(this).remove(); // ($this) = li
+    }); 
+    event.stopPropagation(); // stop bubbeling algorithm for parent elements(tags ul, div, body..)
+    
+});
