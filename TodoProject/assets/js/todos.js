3f8a2477ad254536397974be8f1212c313fd95c3
Nikolas9896 <nhlynka@gmail.com>
2019-08-31 21:20:22 +0300

    make input interface

--- TodoProject/assets/js/todos.js
+++ TodoProject/assets/js/todos.js
@@ -11,3 +11,17 @@ $("span").click(function(event){
     event.stopPropagation(); // stop bubbeling algorithm for parent elements(tags ul, div, body..)
     
 });
+//Add new todo
+$("input[type='text']").keypress(function(event){
+
+   if(event.which === 13)
+   {
+       //grabbing new todo text from input
+      var todoText = $(this).val();
+        $(this).val("");
+      // create new li and add to ul
+     
+     $("ul").append("<li><span>X</span> " + todoText + "</li>");
+   }
+
+});
