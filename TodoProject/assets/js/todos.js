b9a63c6f74a585ba46154fcc19d41aa27f2969c0
Nikolas9896 <nhlynka@gmail.com>
2019-08-31 16:43:09 +0300

    add if statment for complited or uncomplited task

--- TodoProject/assets/js/todos.js
+++ TodoProject/assets/js/todos.js
@@ -1,8 +1,18 @@
 // Check Off Specific Todos By Clicking
 $("li").click(function(){
-    $(this).css({
-        color: "gray",
-        textDecoration: "line-through"
-    });
+    if($(this).css("color") === "rgb(128, 128, 128)")
+    {
+        $(this).css({
+            color: "rgb(0, 0, 0)",
+            textDecoration: "none"
+        });
+    }
+    else
+    {
+        $(this).css({
+            color: "rgb(128, 128, 128)",
+            textDecoration: "line-through"
+        });
+    }
 
 });
